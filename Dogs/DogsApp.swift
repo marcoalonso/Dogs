1b37ef7d0a2a36560c16a3ec76417a1139d9e326
Marco Alonso Rodriguez <marcoalonsorota@hotmail.com>
2025-06-06 10:56:30 -0600

    Initial Commit

---
+++ Dogs/DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsApp.swift
+//  Dogs
+//
+//  Created by Marco Alonso Rodriguez on 06/06/25.
+//
+
+import SwiftUI
+
+@main
+struct DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
