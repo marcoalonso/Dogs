94dcd7f6262280daca4936e273c1b82e5dc38f64
Marco Alonso Rodriguez <marcoalonsorota@hotmail.com>
2025-06-06 15:04:45 -0600

    Feature: DogRemoteDataSource created

---
+++ Dogs/DogRemoteDataSource.swift
@@ -0,0 +1,25 @@
+//
+//  DogRemoteDataSource.swift
+//  Dogs
+//
+//  Created by Marco Alonso Rodriguez on 06/06/25.
+//
+
+import Foundation
+
+protocol DogRemoteDataSourceProtocol {
+    func fetchDogs() async throws -> [DogDTO]
+}
+
+final class DogRemoteDataSource: DogRemoteDataSourceProtocol {
+    private let urlString = "https://jsonblob.com/api/1151549092634943488"
+
+    func fetchDogs() async throws -> [DogDTO] {
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode([DogDTO].self, from: data)
+    }
+}
